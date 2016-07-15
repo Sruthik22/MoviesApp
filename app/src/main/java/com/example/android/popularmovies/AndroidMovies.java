c32e9199159929f8ffa9274f635eaf689fc18946
Sruthik22 <fllheroes638@gmail.com>
2016-07-15 19:53:40 -0400

    Add different fields to the AndroidMovie Class

--- app/src/main/java/com/example/android/popularmovies/AndroidMovies.java
+++ app/src/main/java/com/example/android/popularmovies/AndroidMovies.java
@@ -2,10 +2,18 @@ package com.example.android.popularmovies;
 
 public class AndroidMovies {
     String image;
+    String releaseDate;
+    String title;
+    String voteAverage;
+    String plot;
 
-    public AndroidMovies(String image)
+    public AndroidMovies(String image, String releaseDate, String title, String voteAverage, String plot)
     {
         this.image = image;
+        this.releaseDate = releaseDate;
+        this.title = title;
+        this.voteAverage = voteAverage;
+        this.plot = plot
     }
 
 }
