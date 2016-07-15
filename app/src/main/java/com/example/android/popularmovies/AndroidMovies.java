5f0a23f951f32a2b104909535a818f71bdda8cfc
Sruthi Kurada <fllheroes638@gmail.com>
2016-07-14 22:17:05 -0400

    first commit

---
+++ app/src/main/java/com/example/android/popularmovies/AndroidMovies.java
@@ -0,0 +1,11 @@
+package com.example.android.popularmovies;
+
+public class AndroidMovies {
+    int image; // drawable reference id
+
+    public AndroidMovies(int image)
+    {
+        this.image = image;
+    }
+
+}
---
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -0,0 +1,203 @@
+package com.example.android.popularmovies;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieAdapter extends ArrayAdapter<AndroidMovies> {
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the List is the data we want
+     * to populate into the lists
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param androidMovies A List of AndroidFlavor objects to display in a list
+     */
+    public MovieAdapter(Activity context, List<AndroidMovies> androidMovies) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, androidMovies);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position    The AdapterView position that is requesting a view
+     * @param convertView The recycled view to populate.
+     *                    (search online for "android view recycling" to learn more)
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Gets the AndroidFlavor object from the ArrayAdapter at the appropriate position
+        AndroidMovies androidMovies = getItem(position);
+
+        // Adapters recycle views to AdapterViews.
+        // If this is a new View object we're getting, then inflate the layout.
+        // If not, this view already has the layout inflated from a previous call to getView,
+        // and we modify the View widgets as usual.
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.movie_item, parent, false);
+        }
+
+        ImageView iconView = (ImageView) convertView.findViewById(R.id.androidMovieImage);
+        iconView.setImageResource(androidMovies.image);
+
+        return convertView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getMovieDataFromJson(String json)
+                throws JSONException {
+
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray movies = jsonObject.getJSONArray("results");
+
+            List<String> images = new ArrayList<String>();
+
+            for (int i=0; i<movies.length(); i++) {
+                JSONObject movie = movies.getJSONObject(i);
+                String poster_path = movie.getString("poster_path");
+                images.add(poster_path);
+            }
+
+            for (String s : images) {
+                Log.v(LOG_TAG, "Movie entry: " + s);
+            }
+
+            /*String[] image = new String[images.size()];
+            int position = 0;
+            for (String s : images) {
+                image[position++] = s;
+
+            }*/
+            String[] image = images.toArray(new String[0]);
+            return image;
+
+        }
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            // If there's no zip code, there's nothing to look up.  Verify size of params.
+            if (params.length == 0) {
+                return null;
+            }
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL =
+                        "https://api.themoviedb.org/3/movie/";
+                final String QUERY_PARAM = "popular";
+                final String APPID_PARAM = "?api_key=";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(APPID_PARAM, "08cad5f78a9c82e7729dc841d27b45f2")
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Forecast string: " + forecastJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieDataFromJson(forecastJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
+    }
+}
